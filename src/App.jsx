6162fe9fbd1232c67ef56abdd71739e6b768e961
ALEF AMORIM DOS SANTOS <alef46073@fiec.edu.br>
2026-09-09 15:52:31 -0300

    refactor:Alterações no tamanho do titulo e da descrição do PlaylistCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Sidebar from "./components/SideBar"
-import PlaylistGrid from './components/PlaylistGrid';
-import Header from './components/Header'
+import Sidebar from "./components/SideBar";
+import PlaylistGrid from "./components/PlaylistGrid";
+import Header from "./components/Header";
 import Player from "./components/Player";
 import Button from "./components/Button";
 import CompactCard from "./components/CompactCard";
@@ -11,45 +11,52 @@ function App() {
   return (
     <div className="layout-principal flex flex-col  w-screen text-white bg-zinc-950 h-screen">
       <div className="flex flex-1 overflow-hidden ">
-          <Sidebar />
+        <Sidebar />
         <main className="flex-1 /*flex-col*/ gap-5 h-full w-full m-2 p-6 pt-0 bg-zinc-900 rounded-lg overflow-y-auto">
-          <Header/>
+          <Header />
           <div className="flex  flex-row gap-3 pt-10 flex-wrap">
-            <CompactCard title="Good Vibes"/>
-            <CompactCard title="Music to play"/>
-            <CompactCard title="Lo-fi"/>
-            <CompactCard title="Top 10"/>
+            <CompactCard title="Good Vibes" />
+            <CompactCard title="Music to play" />
+            <CompactCard title="Lo-fi" />
+            <CompactCard title="Top 10" />
           </div>
-          <section className="h-full pt-5">
+          <section className="h-max pt-5">
             <header>
-              <h2 className="text-2xl font-bold text-white mb-6">Feito para você</h2>
+              <h2 className="text-2xl font-bold text-white mb-6">
+                Feito para você
+              </h2>
             </header>
-            <PlaylistGrid playlistData={playlistData.filter(data => data.type === "podcast")}/>
+            <PlaylistGrid
+              playlistData={playlistData.filter(
+                (data) => data.type === "podcast",
+              )}
+            />
           </section>
           <section className="flex flex-col gap-5 h-max pt-5 pb-5">
             {/* Header da seção */}
             <header>
               <h2 className="text-2xl font-bold text-white mb-6">Explorar</h2>
               <div className="flex  gap-2 justify-evenly">
-                {
-                  generos.map(genero => (
-                    <span className={`min-w-30 p-2 text-center rounded-4xl border-2 font-bold hover:scale-105  hover:bg-gray-400/20 cursor-pointer  transition-transform 
-                    ${genero.selected?"bg-white text-zinc-900 hover:text-white":""}  
-                    `} key={genero.label}>{genero.label}</span>
-                  ))
-                }
+                {generos.map((genero) => (
+                  <span
+                    className={`min-w-30 p-2 text-center rounded-4xl border-2 font-bold hover:scale-105  hover:bg-gray-400/20 cursor-pointer  transition-transform 
+                    ${genero.selected ? "bg-white text-zinc-900 hover:text-white" : ""}  
+                    `}
+                    key={genero.label}
+                  >
+                    {genero.label}
+                  </span>
+                ))}
               </div>
             </header>
-            <PlaylistGrid playlistData={playlistFyData}/>
+            <PlaylistGrid playlistData={playlistFyData} />
           </section>
-        <Button>
-          Play
-        </Button>
+          <Button>Play</Button>
         </main>
       </div>
-      <Player/> 
+      <Player />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/PlaylistCard/index.jsx
+++ src/components/PlaylistCard/index.jsx
@@ -1,15 +1,15 @@
 function PlaylistCard({title, description, imgUrl}) {
     return (
-        <div className="group card relative flex flex-col max-w-60 p-2 min-h-80 items-center  bg-zinc-800  text-white rounded-lg gap-4 cursor-pointer hover:bg-zinc-700 hover:scale-102  transition-colors ">
+        <div className="group card relative flex flex-col max-w-60 p-2 min-h-80 items-center  bg-zinc-800/20  text-white rounded-lg gap-4 cursor-pointer hover:bg-zinc-700 hover:scale-102  transition-colors ">
             <div className="image-wrapper max-w-full hover:bg-zinc-800 transition-colors">
                 <img src={imgUrl || "/imagem-card-default.png"} className="rounded-md" alt="Capa playlist padrão" />
             </div>
             <div className="info-musica flex flex-col justify-end">
-                <div className="w-15 h-15 content-center text-center font-semibold rounded-full absolute bg-green-700 left-40 top-50  scale-0 group-hover:scale-100 transition-transform">
+                <div className="w-15 h-15 content-center text-center font-semibold rounded-full absolute bg-green-700 left-40 top-50  scale-0 group-hover:scale-100 hover:bg-green-800 transition-transform">
                     Play
                 </div>
-                <h2 className="titulo font-semibold text-xl">{title}</h2>
-                <p className="descricao text-balance">{description}</p>
+                <h2 className="titulo font-semibold text-lg text-balance text-zinc-200">{title}</h2>
+                <p className="descricao text-balance  font-semibold text-sm text-zinc-400">{description}</p>
             </div>
         </div>
     );
